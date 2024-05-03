d0cd4cffde23ab79d8bfa01c6d6a65079e2d1bd1
VictorDolzan <victorddf5@gmail.com>
2024-05-03 13:22:35 -0300

    📜Created the Calculator.cs class with CalculatorTests.cs to test it

---
+++ Calculator.cs
@@ -0,0 +1,6 @@
+﻿namespace Utilities;
+
+public static class Calculator
+{
+    public static int Sum(int num1, int num2) => num1 + num2;
+}
---
+++ UtilitiesTests/CalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Utilities;
+
+namespace UtilitiesTests;
+
+[TestFixture]
+public class CalculatorTests
+{
+    [TestCase(1, 2, 3)]
+    [TestCase(1, -1, 0)]
+    [TestCase(0, 0, 0)]
+    [TestCase(100, -50, 50)]
+    [TestCase(11, 12, 23)]
+    public void Sum_ShallAddNumbersCorrectly(int num1, int num2, int expected)
+    {
+        var result = Calculator.Sum(num1, num2);
+        Assert.AreEqual(expected, result, $"Adding {num1} to {num2} shall give {expected} but the result was {result}");
+    }
+}
