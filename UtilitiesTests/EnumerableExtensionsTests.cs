aeb5fdb2956be030d01da2a31c76461f6fe1f738
VictorDolzan <victorddf5@gmail.com>
2024-05-03 10:33:00 -0300

    📜Created 2 more tests using TestCase examples

--- UtilitiesTests/EnumerableExtensionsTests.cs
+++ UtilitiesTests/EnumerableExtensionsTests.cs
@@ -19,13 +19,46 @@ public class EnumerableExtensionsTests
     [Test]
     public void SumOfEvenNumbers_ShallReturnNonZeroResult_IfEvenNumbersArePresent()
     {
+        //arrange
         var input = new int[] {3, 1, 4, 6, 7};
 
+        //act
         var result = input.SumOfEvenNumbers();
 
+        //assert
         var expected = 10;
         var inputAsString = string.Join(", ", input);
-        
         Assert.AreEqual(10, result, $"For input {inputAsString} the result shall be {expected} but it was {result}.");
+    }
+    
+    [TestCase(8, 8)]
+    [TestCase(-8, -8)]
+    [TestCase(6, 6)]
+    [TestCase(0, 0)]
+    public void SumOfEvenNumbers_ShallReturnValueOfTheOnlyItem_IfItIsEven(int number, int expectedResult)
+    {
+        //arrange
+        var input = new int[] { number };
+
+        //act
+        var result = input.SumOfEvenNumbers();
+
+        //assert
+        Assert.AreEqual(expectedResult, result, $"For input {number} the result shall be {expectedResult} but it was {result}.");
+    }
+    
+    [TestCase(1)]
+    [TestCase(-7)]
+    [TestCase(33)]
+    public void SumOfEvenNumbers_ShallReturnZero_IfOnlyNumberInInputIsOdd(int number, int expectedResult = 0)
+    {
+        //arrange
+        var input = new int[] { number };
+
+        //act
+        var result = input.SumOfEvenNumbers();
+
+        //assert
+        Assert.AreEqual(expectedResult, result, $"For input {number} the result shall be {expectedResult} but it was {result}.");
     }
 }
