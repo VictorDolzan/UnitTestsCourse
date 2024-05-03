a39c9f358f2224676ced323967143db94bc2ebc0
VictorDolzan <victorddf5@gmail.com>
2024-05-03 10:11:53 -0300

    📜Created a second test with a message in case it fails

--- UtilitiesTests/EnumerableExtensionsTests.cs
+++ UtilitiesTests/EnumerableExtensionsTests.cs
@@ -15,4 +15,17 @@ public class EnumerableExtensionsTests
         
         Assert.AreEqual(0, result);
     }
+    
+    [Test]
+    public void SumOfEvenNumbers_ShallReturnNonZeroResult_IfEvenNumbersArePresent()
+    {
+        var input = new int[] {3, 1, 4, 6, 7};
+
+        var result = input.SumOfEvenNumbers();
+
+        var expected = 10;
+        var inputAsString = string.Join(", ", input);
+        
+        Assert.AreEqual(10, result, $"For input {inputAsString} the result shall be {expected} but it was {result}.");
+    }
 }
