e159d39e489c3f9670d08f00d0cf140d80d34341
VictorDolzan <victorddf5@gmail.com>
2024-05-10 13:51:13 -0300

    📜Created private method

--- EnumerableExtensions.cs
+++ EnumerableExtensions.cs
@@ -5,7 +5,9 @@ public static class EnumerableExtensions
     public static int SumOfEvenNumbers(this IEnumerable<int> numbers)
     {
         return numbers
-            .Where(number => number % 2 == 0)
+            .Where(IsEven)
             .Sum();
     }
+    
+    private static bool IsEven(int number) => number % 2 == 0;
 }
