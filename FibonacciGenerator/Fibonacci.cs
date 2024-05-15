0293445387e8cd5e2da6afa31d3aa33a49cdf23c
VictorDolzan <victorddf5@gmail.com>
2024-05-15 11:47:54 -0300

    📜Created projects for FibonacciGenerator 📜Created Unit tests for Generator

---
+++ FibonacciGenerator/Fibonacci.cs
@@ -0,0 +1,34 @@
+﻿namespace FibonacciGenerator;
+
+public static class Fibonacci
+{
+    public static IEnumerable<int> Generate(int num)
+    {
+        if (num < 0)
+        {
+            throw new ArgumentException($"{nameof(num)} canno be smaller than 0.");
+        }
+
+        const int MaxValidNumber = 46;
+
+        if (num > MaxValidNumber)
+        {
+            throw new ArgumentException(@$"{nameof(num)} cannot be larger than {MaxValidNumber}, as it will cause numeric overflow");
+        }
+
+        var result = new List<int>();
+
+        int a = -1;
+        int b = 1;
+
+        for (int i = 0; i < num; i++)
+        {
+            int sum = a + b;
+            result.Add(sum);
+            a = b;
+            b = sum;
+        }
+        
+        return result;
+    }
+}
---
+++ FibonacciGeneratorTests/FibonacciGeneratorTests.cs
@@ -0,0 +1,62 @@
+﻿using FibonacciGenerator;
+using NUnit.Framework;
+
+namespace FibonacciGeneratorTests;
+
+[TestFixture]
+public class FibonacciGeneratorTests
+{
+    [TestCase(-1)]
+    [TestCase(-5)]
+    [TestCase(-100)]
+    public void Generate_ShallThrowException_IfNumIsSmallerThanZero(int num)
+    {
+        Assert.Throws<ArgumentException>(() => Fibonacci.Generate(num));
+    }
+    
+    [TestCase(47)]
+    [TestCase(52)]
+    [TestCase(90)]
+    public void Generate_ShallThrowException_IfNumIsLargerThan46(int num)
+    {
+        Assert.Throws<ArgumentException>(() => Fibonacci.Generate(num));
+    }
+    
+    [Test]
+    public void Generate_ShallProduceEmptySequence_ForNumEqualTo0()
+    {
+        var result = Fibonacci.Generate(0);
+        Assert.IsEmpty(result);
+    }
+
+    [Test]
+    public void Generate_ShallProduceSequenceWith_0_ForNumEqualTo1()
+    {
+        var result = Fibonacci.Generate(1);
+        Assert.AreEqual(new int[] { 0 }, result);
+    }
+    
+    [Test]
+    public void Generate_ShallProduceSequenceWith_0_1_ForNumEqualTo2()
+    {
+        var result = Fibonacci.Generate(2);
+        Assert.AreEqual(new int[] { 0, 1 }, result);
+    }
+
+    [TestCase(3, new int[] { 0, 1, 1 })]
+    [TestCase(5, new int[] { 0, 1, 1, 2, 3 })]
+    [TestCase(10, new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 })]
+    public void Generate_ShallProduceValidFibonacciSequence(int num, int[] expected)
+    {
+        var result = Fibonacci.Generate(num);
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public void Generate_ShallProduceSequenceWithLastNumber_1134903170_ForNumEqualTo46()
+    {
+        var result = Fibonacci.Generate(46);
+        const int FibonacciNumber46 = 1134903170;
+        Assert.AreEqual(FibonacciNumber46, result.Last());
+    }
+}
